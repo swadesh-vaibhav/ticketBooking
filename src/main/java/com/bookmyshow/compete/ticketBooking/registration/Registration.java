05a59bf04864939610564699c6d1ccc963b5b5c9
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-12-03 07:48:24 +0530

    Removed MongoDB annotations

--- src/main/java/com/bookmyshow/compete/ticketBooking/registration/Registration.java
+++ src/main/java/com/bookmyshow/compete/ticketBooking/registration/Registration.java
@@ -2,11 +2,8 @@ package com.bookmyshow.compete.ticketBooking.registration;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("registrations")
-public record Registration(@Id Integer id,
+public record Registration(Integer id,
                            @NotNull(message = "Please provide product ID") Integer productId,
                            String ticketCode,
                            @NotBlank(message = "Attendee Name cannot be blank") String attendeeName) {
