6cf29c64759971c1c811e2066cb9805d94d60f5a
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-12-20 19:58:12 +0530

    Changed ticketId to Ticket Code in Registration Details page

--- ticketBookingFrontend/src/components/RegistrationDetails.jsx
+++ ticketBookingFrontend/src/components/RegistrationDetails.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './RegistrationDetails.css'; // Import the CSS file
 
 const RegistrationDetails = () => {
-  const [ticketId, setTicketId] = useState('');
+  const [ticketCode, setTicketCode] = useState('');
   const [registrationDetails, setRegistrationDetails] = useState(null);
   const [error, setError] = useState(false);
   const location = useLocation();
@@ -17,7 +17,7 @@ const RegistrationDetails = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.get(`http://localhost:8080/registrations/${ticketId}`)
+    axios.get(`http://localhost:8080/registrations/${ticketCode}`)
       .then(response => {
         setRegistrationDetails(response.data);
       })
@@ -33,21 +33,21 @@ const RegistrationDetails = () => {
       <h2>Registration Details</h2>
       {!registrationDetails && (<form onSubmit={handleSubmit}>
         <div>
-          <label>Enter Ticket ID:</label>
+          <label>Enter Ticket Code (Voucher Code):</label>
           <input
             type="text"
-            value={ticketId}
-            onChange={(e) => setTicketId(e.target.value)}
+            value={ticketCode}
+            onChange={(e) => setTicketCode(e.target.value)}
             required
           />
         </div>
         <button type="submit">Get Details</button>
       </form>
       )}
-      {error && <p className="error">Error fetching registration details. Please check the Ticket ID and try again.</p>}
+      {error && <p className="error">Error fetching registration details. Please check the Ticket Code and try again.</p>}
       {registrationDetails && (
         <div className="registration-details">
-          <p><strong>Ticket ID:</strong> {registrationDetails.id}</p>
+          <p><strong>Ticket Code (This is your voucher code):</strong> {registrationDetails.ticket}</p>
           <p><strong>Attendee Name:</strong> {registrationDetails.attendeeName}</p>
           <p><strong>Event Name:</strong> {registrationDetails.eventName}</p>
           <p><strong>Price:</strong> ${registrationDetails.amount.toFixed(2)}</p>
