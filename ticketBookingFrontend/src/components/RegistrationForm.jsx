7050868146d10c58cac98403a8a58aaf4b398d38
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-12-20 18:18:24 +0530

    Formatting Registration Form

--- ticketBookingFrontend/src/components/RegistrationForm.jsx
+++ ticketBookingFrontend/src/components/RegistrationForm.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
+import './RegistrationForm.css'; // Import the CSS file
 
 const RegistrationForm = () => {
   const [productId, setProductId] = useState('');
