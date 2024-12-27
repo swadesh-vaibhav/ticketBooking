79ffe3bf8a2b135ce0d027d0019996adb671c787
swadesh-vaibhav <swadesh.vaibhav@gmail.com>
2024-12-27 12:37:10 +0530

    Added logging to update-service

--- update-service/src/main/java/com/bookmyshow/compete/ticketBooking/handlers/TransactionSuccessHandler.java
+++ update-service/src/main/java/com/bookmyshow/compete/ticketBooking/handlers/TransactionSuccessHandler.java
@@ -3,11 +3,16 @@ package com.bookmyshow.compete.ticketBooking.handlers;
 import com.bookmyshow.compete.ticketBooking.payments.Transaction;
 import com.bookmyshow.compete.ticketBooking.registrations.Registration;
 import com.bookmyshow.compete.ticketBooking.registrations.RegistrationsClient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionSuccessHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionSuccessHandler.class);
 
     private final RegistrationsClient registrationsClient;
 
@@ -17,6 +22,9 @@ public class TransactionSuccessHandler {
 
     @RabbitListener(queues = "payment.success")
     public void handleTransactionSuccess(Transaction transaction) {
+
+        logger.info("Transaction success handler started for transaction ID: {}", transaction.getId());
+
         try {
             Thread.sleep(transaction.getProcessingTime() * 1000L);
         } catch (InterruptedException e) {
@@ -24,6 +32,7 @@ public class TransactionSuccessHandler {
         }
 
         Registration existing = registrationsClient.getRegistrationById(transaction.getRegistrationId());
+        logger.info("Fetched existing registration for transaction ID: {}", transaction.getId());
         Registration updated = new Registration(
                 existing.id(),
                 existing.productId(),
@@ -35,5 +44,9 @@ public class TransactionSuccessHandler {
                 com.bookmyshow.compete.ticketBooking.registrations.RegistrationStatus.CONFIRMED
         );
         registrationsClient.updateRegistration(updated);
+
+        logger.info("Updated registration for transaction ID: {}", transaction.getId());
+
+        logger.info("Transaction success handler completed for transaction ID: {}", transaction.getId());
     }
 }
